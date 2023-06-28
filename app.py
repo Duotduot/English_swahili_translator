14c32061a17c07e0877b26c3410f6b9d393706c2
Duotduot <13892@coderacademy.edu.au>
2023-06-28 18:06:46 +1000

    Error handling

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 translate_client = translate.Client()
 
+# Translation model definition 
 class Translation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     source_text = db.Column(db.Text)
@@ -17,56 +18,77 @@ class Translation(db.Model):
     target_language = db.Column(db.String(2))
     translated_text = db.Column(db.Text)
 
+# Language model definition 
 class Language(db.Model):
     language_id = db.Column(db.String(2), primary_key=True)
     language_name = db.Column(db.String(100))
 
+# Error handling decorator 
+@app.errorhandler(Exception)
+def handle_error(error):
+    response = {
+        'error': 'An unexpected error occurred.'
+    }
+    return jsonify(response), 500
+
+# Translate endpoint 
 @app.route('/translate', methods=['POST'])
 def translate():
-    data = request.get_json()
-    source_text = data.get('source_text')
-    source_language = 'en'
-    target_language = 'sw'
+    try:
+        data = request.get_json()
+        source_text = data.get('source_text')
+        source_language = 'en'
+        target_language = 'sw'
 
-    # Google Cloud Translation API
-    translation = translate_client.translate(
-        source_text, source_language=source_language, target_language=target_language
-    )
+        if not source_text or not source_language or not target_language:
+            abort(400, 'Invalid request. Missing required parameters.')
 
-    translated_text = translation['translatedText']
+        # Google Cloud Translation API
+        translation = translate_client.translate(
+            source_text, source_language=source_language, target_language=target_language
+        )
 
-    response = {
-        'translated_text': translated_text
-    }
-    return jsonify(response)
+        translated_text = translation['translatedText']
 
+        response = {
+            'translated_text': translated_text
+        }
+        return jsonify(response)
+    except Exception as e:
+        abort(500, 'An error occurred during translation.')
+
+# Languages endpoint
 @app.route('/languages', methods=['GET'])
 def get_languages():
+    try:
+        response = translate_client.get_languages()
 
-    response = translate_client.get_languages()
+        languages = []
+        for language in response:
+            language_id = language['language']
+            language_name = language['name']
+            languages.append({'language_id': language_id, 'language_name': language_name})
 
-    languages = []
-    for language in response:
-        language_id = language['language']
-        language_name = language['name']
-        languages.append({'language_id': language_id, 'language_name': language_name})
+            # Connect to the database
+            connection = psycopg2.connect('postgresql://swahili_user:swahiliuser123@localhost/swahili_db')
+            cursor = connection.cursor()
 
-        # Connect to the  database
-        connection = psycopg2.connect('postgresql://swahili_user:swahiliuser123@localhost/swahili_db')
-        cursor = connection.cursor()
+            # Fetch the list of supported languages
+            cursor.execute("SELECT * FROM language")
+            languages = cursor.fetchall()
 
-        # Fetch the list of suppported languages
-        cursor.execute("SELECT * FROM language")
-        languages = cursor.fetchall()
-
-        # Close the database connection
-        cursor.close()
-        connection.close()
+            # Closing the database connection
+            cursor.close()
+            connection.close()
 
         response = {
             'languages': languages
         }
         return jsonify(response)
+    except Exception as e:
+        abort(500, 'An error occurred while retrieving languages.')
+    
+    
     
 if __name__ == '__main__':
     app.run()
